334a35cc8561a4a4e43643cb1e090fcca85bb607
dev-jhon-ss <dev.jhon.ss@gmail.com>
2022-10-03 15:36:36 -0300

    build: :heavy_plus_sign: react router dom
    :

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -5,6 +6,12 @@ import { Button} from '../components/Button'
 import '../styles/auth.scss'
 
 export function Home() {
+  const navigate = useNavigate();
+
+  function navigateToNewRoom() {
+    navigate('/rooms/new');
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -15,7 +22,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className='create-room'>
+          <button onClick={navigateToNewRoom} className='create-room'>
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
